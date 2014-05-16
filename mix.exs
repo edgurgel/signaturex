2c9d8335cb69145c9d76f2e89f87621f6a6085dd
Eduardo Gurgel <eduardo@gurgel.me>
2014-05-16 22:25:38 +1200

    Bump version 0.0.3
    [ci skip]

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule Signaturex.Mixfile do
       name: "Signaturex",
       description: @description,
       elixir: "~> 0.13.1",
-      version: "0.0.2",
+      version: "0.0.3",
       package: package,
       deps: deps ]
   end
