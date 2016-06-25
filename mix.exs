30284f74a30916ce364e1b76263ac64b8e07e622
Eduardo Gurgel <eduardo@gurgel.me>
2016-06-26 11:55:48 +1200

    Bump Elixir requirement to ~> 1.2

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Signaturex.Mixfile do
     [ app: :signaturex,
       name: "Signaturex",
       description: @description,
-      elixir: "~> 1.0",
+      elixir: "~> 1.2",
       version: "1.0.1",
       package: package,
       deps: deps,
